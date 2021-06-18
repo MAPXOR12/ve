a9e805211dddd6274c490754c5de3744e6136933
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-18 14:46:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ client.on("guildCreate" , AQUAMAN => {
   }
 
 client.on("message", message => {
-  if (message.content.startsWith(prefix + "man gif")) {
+  if (message.content.startsWith(PREFIX + "man gif")) {
     let man = [
       "https://media.discordapp.net/attachments/786897044483604490/803870769313480714/Enes_Acar_GIF_70.gif",
       "https://media.discordapp.net/attachments/786897044483604490/803870793716858880/a_57a7f6c875e3a329b170edf177392911.gif",
@@ -179,8 +179,8 @@ client.on("message", message => {
       "https://media.discordapp.net/attachments/786897044483604490/804968381816111124/image0-5.gif",
       "https://media.discordapp.net/attachments/786897044483604490/804760463044640808/ALANIS_MAN_GIF_99.gif",
       "https://media.discordapp.net/attachments/786897044483604490/803870704999202836/ENES_ACAR_GIF_104.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/803870737941135421/ENES_ACAR_GIF_15.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/803870682479067166/ENES_ACAR_GIF_135.gif"
+      "https://media.discordapp.net/attachments/786897044483604490/803870737941135421/ENES_ACAR_GIF_15.gif"
+      
     ];
 
     message.channel
@@ -197,7 +197,6 @@ client.on("message", message => {
       });
   }
 });
-
 
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
